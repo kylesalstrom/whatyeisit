2372f63056c89eec14722770dee8cf5c8eeb821a
Kyle Salstrom <kylesalstrom@gmail.com>
2022-05-02 12:16:57 -0700

    adds social bar

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import YeDisplay from './YeDisplay';
 import DateDisplay from './DateDisplay';
-// import SocialDisplay from './SocialDisplay';
+import SocialDisplay from './SocialDisplay';
 import AuthorDisplay from './AuthorDisplay';
 
 import './App.css';
@@ -11,7 +11,7 @@ export default function App() {
     <div className="App">
       <YeDisplay date= {today}/>
       <DateDisplay date= {today}/>
-      {/*<SocialDisplay/>*/}
+      <SocialDisplay/>
       <AuthorDisplay/>
     </div>
   );
