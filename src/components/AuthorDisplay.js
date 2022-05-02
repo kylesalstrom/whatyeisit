5d63e3ad924bc48610c98ec0976cc30b26771640
Kyle Salstrom <kylesalstrom@gmail.com>
2022-05-02 13:46:37 -0700

    iphone fix

--- src/components/AuthorDisplay.js
+++ src/components/AuthorDisplay.js
@@ -5,13 +5,13 @@ const DateDisplay = () => {
         <div>
             <a href="http://www.itscolortime.com" target="_blank" rel="noreferrer" title="It's Color Time">
                 <SafeImage
-                    fileName={'image/otherPics/nick.png'} 
+                    fileName={'https://d2desgpiu5lrdz.cloudfront.net/image/otherPics/nick.png'} 
                     className='smlImg' 
                     alt='Nick Stokes'/>
             </a>
             <a href="http://cargocollective.com/ericswanson" target="_blank" rel="noreferrer" title="Eric Swanson">
                 <SafeImage
-                    fileName={'image/otherPics/eric.png'} 
+                    fileName={'https://d2desgpiu5lrdz.cloudfront.net/image/otherPics/eric.png'} 
                     className='smlImg' 
                     alt='Eric Swanson'/>
             </a>
--- src/components/YeDisplay.js
+++ src/components/YeDisplay.js
@@ -1,20 +1,48 @@
 import SafeImage from './SafeImage';
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 
 const YeDisplay = ({date}) => {
     const [yeOfWeek, fileExt, isMobile] = initialize(date);
-    const [isMuted, setIsMuted] = useState(!isMobile);
-    const handleClick = () => setIsMuted(!isMobile && !isMuted);
+    const [isMuted, setIsMuted] = useState(true);
+    const handleClick = () => setIsMuted(!isMuted);
+
+    useEffect(() => {
+        if(isMobile){
+            const video = document.getElementById('yeVideo');
+            const isAndroid = navigator.userAgent.toLowerCase().indexOf("android") > -1;
+            video.autobuffer = true;
+            if(isAndroid) {
+                //document.getElementById('imgClickFor').style.visibility = "hidden";
+                //video.setAttribute("src", 'video/'+ye[n]+'_Final_droid.mp4');
+                video.load();
+                video.loop = false; 
+                video.addEventListener('ended', function() { 
+                    video.currentTime=0.1; video.play(); }, false);
+                video.play();
+                video.controls = true;
+                video.autoplay = true;
+                video.muted = false;
+            }
+            else
+            {
+                video.load();
+                video.autoplay = true;
+                video.playsInline = true;
+                video.play();
+            }
+        }
+    }, [isMobile]);
 
     return (
         <div>
             <video 
+                id="yeVideo"
                 onClick={handleClick}
                 src={`https://d2desgpiu5lrdz.cloudfront.net/video/${yeOfWeek}_Final${fileExt}`}
                 muted={isMuted} 
                 loop 
-                autoPlay />
+                autoPlay/>
             <SafeImage
                 onClick={handleClick}
                 id='imgClickFor' 
